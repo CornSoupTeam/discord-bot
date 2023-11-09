43226dcdfa0fdfe14cd9c76f9f2991ca68e442a9
sh4lluloveme <wh3n_gg@kakao.com>
2023-11-09 16:07:12 +0900

    First commit ✨

---
+++ main.js
@@ -0,0 +1,39 @@
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+const fs = require("fs");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once("ready", () => {
+  console.log("시스템 | 정상적으로 로그인 되었습니다.");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "명령어를 실행하는 도중 오류가 발생했습니다.",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(token);
---
+++ modules/dbConnecter.js
@@ -0,0 +1,8 @@
+const userSchema = require("../models/studentInfo");
+
+const { DATABASE_URL } = require("../config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(DATABASE_URL).catch((error) => console.log(error));
+const userController = mongoose.model("user", userSchema);
+module.exports = { userController };
